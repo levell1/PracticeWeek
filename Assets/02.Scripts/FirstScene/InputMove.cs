2d34d36d2c92861a206797f7b4ebbc5d45b524ed
종 욱 <whddnr0317@naver.com>
2023-12-27 21:02:02 +0900

    [ADD] InputSystem 추가

---
+++ Assets/02.Scripts/FirstScene/InputMove.cs
@@ -0,0 +1,102 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputMove : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+    public float jumpForce;
+    public LayerMask groundLayerMask;
+    private Vector2 mouseDelta;
+    private int JumpCount = 1;
+
+    [HideInInspector]
+    public bool canLook = true;
+
+    private Rigidbody _rigidbody;
+
+    public static InputMove instance;
+    private void Awake()
+    {
+        instance = this;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (IsGrounded() && JumpCount != 1)
+        {
+            Debug.Log("3");
+            JumpCount = 1;
+        }
+    }
+    private void FixedUpdate()
+    {
+        Move();
+        
+    }
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = _rigidbody.velocity.y;
+
+        _rigidbody.velocity = dir;
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            if (JumpCount != 0)
+            {
+                JumpCount--;
+                _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+    private bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (Vector3.up * 0.01f) , Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f)+ (Vector3.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (Vector3.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) + (Vector3.up * 0.01f), Vector3.down),
+        };
+
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.1f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position + (transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.forward * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (transform.right * 0.2f), Vector3.down);
+        Gizmos.DrawRay(transform.position + (-transform.right * 0.2f), Vector3.down);
+    }
+}
